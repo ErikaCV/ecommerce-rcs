d21655a54b83e5076e9ff1672afb579213ea1226
JoeTerol <teroljoseeduardo@gmail.com>
2023-04-16 14:33:52 -0300

    new js page, new array wtih products ando new img in assets

---
+++ cards.js
@@ -0,0 +1,203 @@
+const productArray = [
+    {
+        id: "product01",
+        title: "Pechera Radikal Racing",
+        image: "./assets/img/cards/pechera.webp",
+        category: {
+            id: "accesories",
+            name: "Accesorios"
+        },
+        price: 26690
+    },
+    {
+        id: "product02",
+        title: "Frenos Shimano",
+        image: "./assets/img/cards/frenos.webp",
+        category: {
+            id: "accesories",
+            name: "Accesorios"  
+        },
+        price: 90000
+    },
+    {
+        id: "product03",
+        title: "Cubierta de Ruta Maxxis",
+        image: "./assets/img/cards/cubierta.webp",
+        category: {
+            id: "accesories" ,
+            name: "Accesorios"
+        },
+        price: 25600
+    },
+    {
+        id: "product04",
+        title: "Ciclo Compu IGPS Sports" ,
+        image: "./assets/img/cards/ciclocompu.webp",
+        category: {
+            id: "accesories",
+            name: "Accesorios"
+        },
+        price: 64500
+    },
+    {
+        id: "product05",
+        title: "Sensor de Velocidad IGPS Sport",
+        image: "./assets/img/cards/sensor velocidad.webp", 
+        category: {
+            id: "accesories",
+            name: "Accesorios"
+        },
+        price: 13200
+    },
+    {
+        id: "product06",
+        title: "Guantes Troy Lee",
+        image: "./assets/img/cards/guantes1.webp",
+        category: {
+            id:"dress",
+            name: "Indumentaria"
+        },
+            price: 14900
+    },
+    {
+        id: "product07",
+        title: "Calza Aero 03",
+        image: "./assets/img/cards/calza.webp",
+        category: {
+            id: "dress",
+            name: "Indumentaria"
+        },
+        price: 19900
+    },
+    {
+        id: "product08",
+        title: "Blue Jersey Zeroox",
+        image: "./assets/img/cards/jersey blue front.webp",
+        category: {
+            id: "dress",
+            name: "Indumentaria"
+        },
+        price: 13800
+    },
+    {
+        id: "product09",
+        title: "Jersey Dama Designs Lilium",
+        image: "./assets/img/cards/jerseydama.webp",
+        category: {
+            id: "dress" ,
+            name: "Indumentaria"
+        },
+        price: 24500
+    },
+    {
+        id: "product10",
+        title: "Rompevientos Magenta - Negro",
+        image: "./assets/img/cards/rompeviento",
+        category: {
+            id: "dress" ,
+            name: "Indumentaria"
+        },
+        price: 20000
+    },
+    {
+        id: "product11",
+        title:"Zenith Urban - Amarillo",
+        image: "./assets/img/cards/urbanazenith ffw amarillo.webp",
+        category: {
+            id: "bikes",
+            name: "Bicicletas"
+        },
+        price: 159200
+    },
+    {
+        id: "product12",
+        title: "Zenith Andes - Negro",
+        image: "./assets/img/cards/zenith",
+        category: {
+            id: "bikes",
+            name: "Bicicletas"
+        },
+        price: 184900
+    },
+    {
+        id: "product13",
+        title: "Camicleta Niño Rembrandt - Lima",
+        image: "./assets/img/cards/rembrandt jumper.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 33500
+    },
+    {
+        id: "product14",
+        title: "Zenith Riva Elite - Naranja",
+        image: "./assets/img/cards/zenith riva.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price:  339000
+    },
+    {
+        id: "product15",
+        title: "Zenith Calea - Gris",
+        image: "./assets/img/cards/Bicicleta Mtb Zenith.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 411900
+    },
+    {
+        id: "product16",
+        title: "Zenith Atacama - Negro",
+        image: "./assets/img/cards/zenith atacama.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 160000
+    },
+    {
+        id: "product17",
+        title: "Zenith Woman Cima - Gris",
+        image: "./assets/img/cards/woman cima.webps",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 180000
+    },
+    {
+        id: "product18",
+        title: "Zenith Calea Elite - Roja",
+        image: "./assets/img/cards/zenith roja.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 505000
+    },
+    {
+        id: "product19",
+        title: "Zenith Cima c/Suspensión - Azul",
+        image: "./assets/img/cards/zenith con suspension.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 154.900
+    },
+    {
+        id: "product20",
+        title: "Zenith Andes Elite - Celeste",
+        image: "./assets/img/cards/zenith andes elite.webp",
+        category: {
+            id: "bikes" ,
+            name: "bicicletas"
+        },
+        price: 230000
+    }
+
+]
