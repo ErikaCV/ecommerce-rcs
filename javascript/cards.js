643008d9c7aaac9352799bf562c6c6d80efec983
JoeTerol <teroljoseeduardo@gmail.com>
2023-05-20 17:02:53 -0300

    new target blank on products card

--- javascript/cards.js
+++ javascript/cards.js
@@ -28,7 +28,7 @@ function loadproducts(chosenProducts) {
 					</div>
 					<div id="filternone" class="card-body d-flex filternone">
 					    <a href="#" id="${product.id}" class="btn add-cart filternone"><i class="bi bi-cart-plus-fill"></i></a>
-					    <a href="../views/card.html" data-productid="${product.id}" class="btn btn-danger p-1 m-1 filternone product">Ir al Producto</a>
+					    <a href="../views/card.html" target="_blank" data-productid="${product.id}" class="btn btn-danger p-1 m-1 filternone product">Ir al Producto</a>
 					</div>
         `;
         containerProducts.append(div); 
